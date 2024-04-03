b8eea0945a459fcd00e06d2e948fff11a6f9ff57
Khiem Le <87161814+Khiem17204@users.noreply.github.com>
2024-04-03 00:00:42 -0400

    dep trai

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -1,5 +1,5 @@
 
-const address_cat_pic = []
+const address_cat_pic = ["../../../figures/cat_pic/cat1.jpg", "../../../figures/cat_pic/cat2.jpg", "../../../figures/cat_pic/cat3.jpg", "../../../figures/cat_pic/cat4.jpg", "../../../figures/cat_pic/cat5.jpg", "../../../figures/cat_pic/cat6.jpg", "../../../figures/cat_pic/cat7.jpg", "../../../figures/cat_pic/cat8.jpg", "../../../figures/cat_pic/cat9.jpg", "../../../figures/cat_pic/cat10.jpg"]
 
 let cat_button = document.getElementById("change-cat")
 let random_cat_image = document.getElementById("random-cat-image")
@@ -10,5 +10,6 @@ function cat_click(){
     let random_index = Math.floor(Math.random()*10)
     new_img.src = address_cat_pic[random_index]
     new_img.alt = "random cat picture"
+    new_img.id = "random-cat-image"
     random_cat_image.append(new_img)
 }
