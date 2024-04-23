66452231ce198b24954c30dbc20b549fa242f546
Khiem Le <87161814+Khiem17204@users.noreply.github.com>
2024-04-23 15:17:23 -0400

    ok

--- Milestone2+3/src/client/script/script.js
+++ Milestone2+3/src/client/script/script.js
@@ -1,3 +1,21 @@
-var db = new PouchDB('user');
+import PouchDB from "pouchdb";
+import { Available } from "../utils/available";
+import { User } from "../utils/user";
+
+
+const test_available = [new Available("tung", "cho", 3, "Female", "cat"), new Available("Minh", "gau", 3, "Male", "cat"), new Available("Minh", "gau", 3, "Male", "cat") ]
+
+const test_user = new User("ktle@umass.edu", "May", 3, "Male", "cat", "Heo", [], "Good cat", "eat and sleep", "I only want rich cat")
+
+
+
+const db = new PouchDB('user');
+
+
+async function saveUser(user){
+    await db.put({_id: 1, user})
+}
+
+
 
 
