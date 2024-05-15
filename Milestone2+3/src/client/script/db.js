b8543bdfb0649050a6d84e7629672221e480133f
Huy Cao <caogiahuy615@gmail.com>
2024-05-15 01:14:16 -0400

    huy dep trai

--- Milestone2+3/src/client/script/db.js
+++ Milestone2+3/src/client/script/db.js
@@ -1,31 +1,29 @@
-// import PouchDB from './pouchdb/lib/index.es.js';
+const db = new PouchDB('user');
 
-// const db = new PouchDB('userClient');
-
-// export async function saveUser(user){
-//     try{
-//         await db.put({_id: user.getId(), ...user})
-//         console.log("save user succesfully")
-//     }catch(e){
-//         console.log(e)
-//     }
+export async function saveUser(user){
+     try{
+         await db.put({_id: user.getId(), ...user})
+         console.log("save user succesfully")
+     }catch(e){
+         console.log(e)
+     }
     
-// }
+}
 
-// export async function getUser(){
-//     const user = await db.allDocs()
-//     console.log(user)
-//     console.log(user.rows)
-//     return user.rows[0].doc
-// }
+ export async function getUser(){
+        const user = await db.allDocs()
+        console.log(user)
+        console.log(user.rows)
+        return user.rows[0].doc
+ }
 
-// export async function saveAvailableProfile(available){
-//     try {
-//         for (const e of available){
-//             await db.put({ _id: e.getId(), ...e });
-//         }
-//         console.log("save profile successfully")
-//     }catch(e){
-//         console.log(e)
-//     }
-// }
+ export async function saveAvailableProfile(available){
+     try {
+         for (const e of available){
+             await db.put({ _id: e.getId(), ...e });
+         }
+         console.log("save profile successfully")
+     }catch(e){
+         console.log(e)
+     }
+}
